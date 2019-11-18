43d2fc8bbfc1407d1f1e5a94ce69110fdd411805
Fernanda Tempesta <fernandatempesta@dcc.ufmg.br>
2019-11-18 14:59:44 -0300

    Update kprototypes.py

--- kmodes/kprototypes.py
+++ kmodes/kprototypes.py
@@ -420,7 +420,7 @@ class KPrototypes(kmodes.KModes):
 
     """
 
-    def __init__(self, n_clusters=8, max_iter=100, num_dissim=chebyshev_dissim,
+    def __init__(self, n_clusters=8, max_iter=100, num_dissim=euclidean_dissim,
                  cat_dissim=matching_dissim, init='Huang', n_init=10, gamma=None,
                  verbose=0, random_state=None, n_jobs=1):
 
